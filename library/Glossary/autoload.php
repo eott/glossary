e5dbb9750d0837d09e30a6b5a6e09c96fb41ff17
Etienne Ott <etienne.ott@wiethe.com>
2016-01-08 15:45:59 +0100

    Fix Glossary autoloader to only load classes within the Glossary namespace.
    This is necessary for classes that do not utilize namespaces or that are not in the expected
    directory structure.

--- library/Glossary/autoload.php
+++ library/Glossary/autoload.php
@@ -1,11 +1,15 @@
 <?php
 
 /**
- * Register an autoloader for glossary classed by their namespace.
+ * Register an autoloader for glossary classes by their namespace. Only
+ * classes within the namespace Glossary are required, others are ignored
+ * by this autoloader.
  *
  * @todo Use a generated classmap here; also support multiple/internal classes
  *    in one file
  */
 spl_autoload_register(function($name) {
-    require_once (__DIR__ . '/../' . str_replace('\\', '/', $name) . '.php');
+    if (strcasecmp(substr(ltrim($name, "\\"), 0, 8), 'Glossary') === 0) {
+        require_once (__DIR__ . '/../' . str_replace('\\', '/', $name) . '.php');
+    }
 });
