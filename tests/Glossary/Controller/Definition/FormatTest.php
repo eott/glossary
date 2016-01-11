f489d37d1eb0ee4b348f6c54cdd151016022d663
Etienne Ott <etienne.ott@wiethe.com>
2016-01-11 11:44:41 +0100

    Add tests for class Glossary\Definition\Format.

---
+++ tests/Glossary/Controller/Definition/FormatTest.php
@@ -0,0 +1,34 @@
+<?php
+use \Glossary\Definition\Format;
+
+class FormatTest extends PHPUnit_Framework_TestCase
+{
+    public function testCleanTerm()
+    {
+        $this->assertEquals("foo-bar", Format::cleanTerm("foo bar"));
+        $this->assertEquals("foobar", Format::cleanTerm("foo\t\r\nbar"));
+    }
+
+    public function testFormatDescriptionUnknownTerms()
+    {
+        // Note: Because we expect no known terms here, we must use obscure
+        // test terms, that will most likely never get a definition
+        $input = "Duai289fh doasn230:\n"
+            . "\t-bnsb0243\n"
+            . "\t-lgj23834";
+
+        $output = "Duai289fh doasn230:<br/>"
+            . "-bnsb0243<br/>"
+            . "-lgj23834";
+
+        $this->assertEquals($output, Format::formatDescription($input));
+    }
+
+    public function testFormatDescriptionKnownTerms()
+    {
+        // Note: We must terms here, that we know will exist in the environment
+        $input = "f9823hf92hrf Begriff ";
+        $output = "f9823hf92hrf <span class=\"termLink\" data-term=\"Begriff\">Begriff</span> ";
+        $this->assertEquals($output, Format::formatDescription($input));
+    }
+}
