56ad673cc50d52d1a38552c2cc7ef9615e0c6102
Etienne Ott <etienne.ott@wiethe.com>
2016-01-11 15:21:47 +0100

    Add database schema files for creating the database structure.

---
+++ schema/definition.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `definition` (
+  `definition_id` int(11) NOT NULL AUTO_INCREMENT,
+  `term` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`definition_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
