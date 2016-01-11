45a6f5eb19f1abd6b25227fec976307273e59e54
Etienne Ott <etienne.ott@wiethe.com>
2016-01-11 10:32:30 +0100

    Extends tests and add phpunit.xml for IDEs that support automatic test integration.

--- tests/Glossary/ViewTest.php
+++ tests/Glossary/ViewTest.php
@@ -13,4 +13,22 @@ class ViewTest extends PHPUnit_Framework_TestCase
         $view = new \Glossary\View();
         $this->assertEquals($view->url('//cdn.example.org'), '//cdn.example.org');
     }
+
+    public function testUrlLineNoise()
+    {
+        $view = new \Glossary\View();
+        $this->assertEquals($view->url('#t348hf23+23ß+fi2<1^211´4ß234#+-we,f+'), '/glossary/#t348hf23+23ß+fi2<1^211´4ß234#+-we,f+');
+    }
+
+    public function testUrlInternalWithSlash()
+    {
+        $view = new \Glossary\View();
+        $this->assertEquals($view->url('/definition/show/foobar'), '/glossary/definition/show/foobar');
+    }
+
+    public function testUrlInternalWithoutSlash()
+    {
+        $view = new \Glossary\View();
+        $this->assertEquals($view->url('definition/show/foobar'), '/glossary/definition/show/foobar');
+    }
 }
