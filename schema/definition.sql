8811147353f7cc08acd1ca4b01209058032c4233
Etienne Ott <etienne.ott@wiethe.com>
2016-01-11 16:05:52 +0100

    Fix missing database name in definition creation script.

--- schema/definition.sql
+++ schema/definition.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `definition` (
+CREATE TABLE IF NOT EXISTS `glossary`.`definition` (
   `definition_id` int(11) NOT NULL AUTO_INCREMENT,
   `term` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci,
