62b924a1e808e1d403595b23914879cc882ba7c1
Etienne Ott <etienne.ott@wiethe.com>
2016-01-11 13:48:06 +0100

    Fix tests broken by rewriting Glossary\View class to use base URL from config.

--- tests/Glossary/ViewTest.php
+++ tests/Glossary/ViewTest.php
@@ -2,33 +2,40 @@
 
 class ViewTest extends PHPUnit_Framework_TestCase
 {
+    protected $_view;
+
+    protected function setUp()
+    {
+        $this->_view = new \Glossary\View();
+        $this->_view->setBaseUrl('/glossary');
+    }
+
     public function testUrlExternalUnchanged()
     {
-        $view = new \Glossary\View();
-        $this->assertEquals($view->url('http://www.example.org'), 'http://www.example.org');
+        $this->assertEquals($this->_view->url('http://www.example.org'), 'http://www.example.org');
     }
 
     public function testUrlExternalUnchangedShort()
     {
         $view = new \Glossary\View();
-        $this->assertEquals($view->url('//cdn.example.org'), '//cdn.example.org');
+        $this->assertEquals($this->_view->url('//cdn.example.org'), '//cdn.example.org');
     }
 
     public function testUrlLineNoise()
     {
         $view = new \Glossary\View();
-        $this->assertEquals($view->url('#t348hf23+23ß+fi2<1^211´4ß234#+-we,f+'), '/glossary/#t348hf23+23ß+fi2<1^211´4ß234#+-we,f+');
+        $this->assertEquals($this->_view->url('#t348hf23+23ß+fi2<1^211´4ß234#+-we,f+'), '/glossary/#t348hf23+23ß+fi2<1^211´4ß234#+-we,f+');
     }
 
     public function testUrlInternalWithSlash()
     {
         $view = new \Glossary\View();
-        $this->assertEquals($view->url('/definition/show/foobar'), '/glossary/definition/show/foobar');
+        $this->assertEquals($this->_view->url('/definition/show/foobar'), '/glossary/definition/show/foobar');
     }
 
     public function testUrlInternalWithoutSlash()
     {
         $view = new \Glossary\View();
-        $this->assertEquals($view->url('definition/show/foobar'), '/glossary/definition/show/foobar');
+        $this->assertEquals($this->_view->url('definition/show/foobar'), '/glossary/definition/show/foobar');
     }
 }
