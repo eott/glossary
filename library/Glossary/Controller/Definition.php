1012247caaea74f6522684f483ad1e809e97d41a
Etienne Ott <etienne.ott@wiethe.com>
2016-01-11 11:00:31 +0100

    Move methods for formating Definitions into own class.

--- library/Glossary/Controller/Definition.php
+++ library/Glossary/Controller/Definition.php
@@ -1,6 +1,7 @@
 <?php
 namespace Glossary\Controller;
 
+use \Glossary\Definition\Format;
 /**
  * Handles request about definitions.
  */
@@ -33,12 +34,12 @@ class Definition extends \Glossary\Controller\AbstractController
      */
     public function defineAction($args)
     {
-        $term = $this->cleanTerm(urldecode(reset($args)));
+        $term = Format::cleanTerm(urldecode(reset($args)));
         $filename = APPLICATION_PATH . '/data/' . $term . '.json';
         if (file_exists($filename)) {
             $data = json_decode(file_get_contents($filename));
             $this->_view->mainTerm = $data->term;
-            $this->_view->mainDescription = $this->formatDescription($data->description);
+            $this->_view->mainDescription = Format::formatDescription($data->description);
         } else {
             $this->_view->errors = array('Suchbegriff nicht gefunden.');
         }
@@ -46,7 +47,7 @@ class Definition extends \Glossary\Controller\AbstractController
 
     public function ajaxAction($args)
     {
-        $term = $this->cleanTerm(urldecode(reset($args)));
+        $term = Format::cleanTerm(urldecode(reset($args)));
         $filename = APPLICATION_PATH . '/data/' . strtolower($term) . '.json';
         if (file_exists($filename)) {
             $data = json_decode(file_get_contents($filename));
@@ -63,57 +64,5 @@ class Definition extends \Glossary\Controller\AbstractController
                 </div>";
         }
         exit;
-    }
-
-    /**
-     * Cleans the given seach term for its use in the filename based system.
-     *
-     * @param string $term The search term
-     * @return string The cleaned search term
-     */
-    public function cleanTerm($term)
-    {
-        $search  = array('\r', '\n', '\t', ' ');
-        $replace = array('', '', '', '-');
-        $str     = strtolower(str_replace($search, $replace, $term));
-        $str     = preg_replace('[^\w-]', '', $str);
-        return $str;
-    }
-
-    /**
-     * Formats the given description for its use in the output.
-     * This also looks for other key words in the description and highlights
-     * them for the user, so they can choose to load the definition.
-     *
-     * @todo Extract search for keywords into creation of cards or regular
-     *    check. This is too costly to do on every request.
-     * @todo The key word search does not work properly yet. This probably
-     *    needs an external library for matching words in text
-     * @param string $desc The description
-     * @param string The formatted description
-     */
-    public function formatDescription($desc)
-    {
-        $search  = array('\r', '\n', '\t');
-        $replace = array('', '<br/>', '');
-        $str = str_replace($search, $replace, $desc);
-
-        $matches = array();
-        $pattern = '#\s(\w+)[\s\.,!\?"\']#m';
-        preg_match_all($pattern, $str, $matches);
-
-        if (isset($matches[1])) {
-            foreach ($matches[1] as $match) {
-                $trimmed = trim($match);
-                $term = $this->cleanTerm($trimmed);
-
-                $filename = APPLICATION_PATH . '/data/' . strtolower($term) . '.json';
-                if (file_exists($filename)) {
-                    $str = str_replace($trimmed, '<span class="termLink" data-term="' . $trimmed . '">' . $trimmed . '</span>', $str);
-                }
-            }
-        }
-
-        return $str;
     }
 }
---
+++ library/Glossary/Definition/Format.php
@@ -0,0 +1,60 @@
+<?php
+namespace Glossary\Definition;
+
+/**
+ *
+ */
+class Format
+{
+    /**
+     * Cleans the given seach term for its use in the filename based system.
+     *
+     * @param string $term The search term
+     * @return string The cleaned search term
+     */
+    public static function cleanTerm($term)
+    {
+        $search  = array('\r', '\n', '\t', ' ');
+        $replace = array('', '', '', '-');
+        $str     = strtolower(str_replace($search, $replace, $term));
+        $str     = preg_replace('[^\w-]', '', $str);
+        return $str;
+    }
+
+    /**
+     * Formats the given description for its use in the HTML output.
+     * This also looks for other key words in the description and highlights
+     * them for the user, so they can choose to load the definition.
+     *
+     * @todo Extract search for keywords into creation of cards or regular
+     *    check. This is too costly to do on every request.
+     * @todo The key word search does not work properly yet. This probably
+     *    needs an external library for matching words in text
+     * @param string $desc The description
+     * @param string The formatted description
+     */
+    public static function formatDescription($desc)
+    {
+        $search  = array('\r', '\n', '\t');
+        $replace = array('', '<br/>', '');
+        $str = str_replace($search, $replace, $desc);
+
+        $matches = array();
+        $pattern = '#\s(\w+)[\s\.,!\?"\']#m';
+        preg_match_all($pattern, $str, $matches);
+
+        if (isset($matches[1])) {
+            foreach ($matches[1] as $match) {
+                $trimmed = trim($match);
+                $term = self::cleanTerm($trimmed);
+
+                $filename = APPLICATION_PATH . '/data/' . strtolower($term) . '.json';
+                if (file_exists($filename)) {
+                    $str = str_replace($trimmed, '<span class="termLink" data-term="' . $trimmed . '">' . $trimmed . '</span>', $str);
+                }
+            }
+        }
+
+        return $str;
+    }
+}
