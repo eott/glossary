c97f7a6c4e0db2ecfcf6320f81c9f269829c5894
Etienne Ott <eti.ott@gmail.com>
2016-01-30 18:00:41 +0100

    Use settings for baseUrl instead of hardcoding it in javascript files.

--- scripts/definition.js
+++ scripts/definition.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('body').on('click', '.termLink', function(e) {
         $.ajax({
-            url     : 'http://192.168.7.58/glossary/definition/ajax/' + $(this).data('term'),
+            url     : settings.system.baseUrl + 'definition/ajax/' + $(this).data('term'),
             dataType: 'html',
             data    : {}
         }).done(function (data) {
